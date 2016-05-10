3b23aa46d90d49e087cba93d31a6617041904d49
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-05-10 16:43:12 -0400

    Add demo ops

--- client/CDASRequest.py
+++ client/CDASRequest.py
@@ -4,19 +4,16 @@ import xml.dom.minidom
 server = "localhost"
 port = 9000
 
-def execRequest( requestURL ): return xml.dom.minidom.parseString( urllib2.urlopen( requestURL ).read() )
+class ResultVariable:
 
-# getCapabilitiesReq = getRequestStr( "http://%s:%d/wps?service=cds2&request=GetCapabilities" % ( server, port ) )
-# print getCapabilitiesReq
+    def __init__( self, dom ):
+        self._dom = dom
 
-# describeProcessReq = getRequestStr( "http://%s:%d/wps?service=cds2&request=describeprocess&identifier=cds.average " % ( server, port ) )
-# print describeProcessReq
+    def __str__(self): return self._dom.toprettyxml()
 
-#cacheExecuteReq = getRequestStr( 'http://%s:%s/wps?request=Execute&service=cds2&identifier=util.cache&status=true&datainputs=[domain={"name":"d0","lev":{"start":3,"end":3,"system":"indices"}},variable={"uri":"collection://MERRA/mon/atmos","name":"ta:v0","domain":"d0"}]' % ( server, port ) )
-#print cacheExecuteReq
+    def axes(self): return self._dom.getElementsByTagName('axis')
 
-# anomalyExecuteReq = getRequestStr( 'http://%s:%s/wps?request=Execute&service=cds2&identifier=CDS.anomaly&datainputs=[domain={"name":"d0","lat":{"start":45,"end":45,"system":"values"},"lon":{"start":30,"end":30,"system":"values"},"lev":{"start":3,"end":3,"system":"indices"}},variable={"uri":"collection://MERRA/mon/atmos","name":"ta:v0","domain":"d0"},operation="(v0,axes:t)"]' % ( server, port ) )
-# print anomalyExecuteReq
+def execRequest( requestURL ): return ResultVariable( xml.dom.minidom.parseString( urllib2.urlopen( requestURL ).read() ) )
 
 class Variable:
     def __init__( self, uid, uri, varname, domain_id ):
@@ -28,9 +25,9 @@ class Variable:
     def toWps(self): return '{"uri":"%s","name":"%s:%s","domain":"%s"}' % ( self._uri, self._varname, self._uid, self._dom_id )
 
 class Domain:
-    def __init__( self, id ):
+    def __init__( self, id, axes=[] ):
         self._id = id
-        self._axes = []
+        self._axes = axes
 
     def addAxis( self,  a ): self._axes.append( a )
 
@@ -105,26 +102,139 @@ class CDASExecuteRequest:
     def execute( self, async ):
         request = self.toWps(async)
         print "\nExecuting Request:\n\n%s\n\nResponse:\n" % ( request )
-        return execRequest( request ).toprettyxml()
+        return execRequest( request )
+
+class CDASDemo:
+
+    @classmethod
+    def getTimeseriesDomain( cls, exeReq, levelIndex, id ):
+        d = Domain(id=id)
+        lat = Axis( id="lat", start=45, end=45, system="values")
+        lon = Axis( id="lon", start=30, end=30, system="values")
+        lev = Axis( id="lev", start=levelIndex, end=levelIndex, system="indices")
+        for axis in [lat,lon,lev]: d.addAxis ( axis )
+        exeReq.addDomain( d )
+        return d
+
+    @classmethod
+    def getSpatialDomain( cls, exeReq, levelIndex, id ):
+        d = Domain(id=id)
+        time = Axis( id="time", start=4, end=4, system="indices")
+        lev = Axis( id="lev", start=levelIndex, end=levelIndex, system="indices")
+        for axis in [time,lev]: d.addAxis ( axis )
+        exeReq.addDomain( d )
+        return d
+
+    @classmethod
+    def getColumnDomain( cls, exeReq, id ):
+        d = Domain(id=id)
+        time = Axis( id="time", start=4, end=4, system="indices")
+        lat = Axis( id="lat", start=45, end=45, system="values")
+        lon = Axis( id="lon", start=30, end=30, system="values")
+        for axis in [time,lat,lon]: d.addAxis ( axis )
+        exeReq.addDomain( d )
+        return d
+
+    @classmethod
+    def getCacheDomain( cls, exeReq, levelIndex, id ):
+        d = Domain(id=id)
+        lev = Axis( id="lev", start=levelIndex, end=levelIndex, system="indices")
+        d.addAxis ( lev )
+        exeReq.addDomain( d )
+        return d
+
+    @classmethod
+    def getMerraAtmosVariable( cls, exeReq, uId, varId, domainId ):
+        v = Variable( uid=uId, uri="collection://MERRA/mon/atmos", varname=varId, domain_id=domainId )
+        exeReq.addInputVariable( v )
+        return v
+
+    @classmethod
+    def executeRequest( cls, exeReq, pkg, operation, input_uids, vargs={}, run_async=False ):
+        op = Operation( package=pkg, kernel=operation, input_uids=input_uids, args=vargs )
+        exeReq.addOperation( op )
+        print exeReq.execute( async=run_async )
+
+    @classmethod
+    def executeCacheRequest(cls, levelIndex ):
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getCacheDomain( exeReq, levelIndex, 'dc' )
+        cls.getMerraAtmosVariable( exeReq, "v0", "ta", "dc" )
+        cls.executeRequest( exeReq, "util", "cache", [ "v0"] )
+
+    @classmethod
+    def executeAnomalyConcise(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getTimeseriesDomain(exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "anomaly", [ "v0"], { "axes":"t" } )
+
+    @classmethod
+    def executeAnomalyVerbose(cls):
+        exeReq = CDASExecuteRequest(server,port)
+        v0 = Variable( uid="v0", uri="collection://MERRA/mon/atmos", varname="ta", domain_id="d0" )
+        exeReq.addInputVariable( v0 )
+        d0 = Domain(id="d0")
+        lat = Axis( id="lat", start=45, end=45, system="values")
+        lon = Axis( id="lon", start=30, end=30, system="values")
+        lev = Axis( id="lev", start=3, end=3, system="indices")
+        for axis in [lat,lon,lev]: d0.addAxis ( axis )
+        exeReq.addDomain( d0 )
+        op = Operation( package="CDS", kernel="anomaly", input_uids=[ "v0"], args={ "axes":"t" } )
+        exeReq.addOperation( op )
+        print exeReq.execute( async=False )
+
+    @classmethod
+    def executeSpatialMax(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getSpatialDomain(exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "max", [ "v0"], { "axes":"xy" } )
+
+    @classmethod
+    def executeTemporalMin(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getTimeseriesDomain(exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "min", [ "v0"], { "axes":"t" } )
+
+    @classmethod
+    def executeColumnSum(cls):
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getColumnDomain(exeReq, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "sum", [ "v0"], { "axes":"z" } )
+
+    @classmethod
+    def executeYearlyMeans(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getTimeseriesDomain( exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "aggregate", [ "v0"], { "axes":"t", "bins": "t|year|ave" } )
+
+    @classmethod
+    def executeYearlyCycle(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getTimeseriesDomain( exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "aggregate", [ "v0"], { "axes":"t", "bins": "t|year|ave|year" } )
+
+    @classmethod
+    def executeSpatialAve(cls, levelIndex):
+        cls.executeCacheRequest(levelIndex)
+        exeReq = CDASExecuteRequest(server,port)
+        cls.getSpatialDomain( exeReq, levelIndex, 'd0' )
+        cls.getMerraAtmosVariable(exeReq, "v0", "ta", "d0" )
+        cls.executeRequest(exeReq, "CDS", "average", [ "v0"], { "axes":"xy", "weights": "cosine" } )
 
 
 if __name__ == "__main__":
-
-    exeReq = CDASExecuteRequest(server,port)
-
-    v0 = Variable( uid="v0", uri="collection://MERRA/mon/atmos", varname="ta", domain_id="d0" )
-    exeReq.addInputVariable( v0 )
-
-    d0 = Domain(id="d0")
-    lat = Axis( id="lat", start=45, end=45, system="values")
-    lon = Axis( id="lon", start=30, end=30, system="values")
-    lev = Axis( id="lev", start=3, end=3, system="indices")
-    for axis in [lat,lon,lev]: d0.addAxis ( axis )
-    exeReq.addDomain( d0 )
-
-    op = Operation( package="CDS", kernel="anomaly", input_uids=[ "v0"], args={ "axes":"t" } )
-    exeReq.addOperation( op )
-
-    print exeReq.execute( async=True )
+    levelIndex = 3
+    CDASDemo.executeSpatialAve(levelIndex)
 
 
